68f84dfaa30039021b9e145461e32ef35080c619
FurqanEmam <furqanemam@gmail.com>
2023-03-31 16:21:51 +0600

    css added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -6,21 +6,23 @@ const Blog = (props) => {
     return (
         <div>
             <div className="blog-container">
-                <img className='cover-img' src={cover} alt="Cover" />
-                <div>
-                    <img className='author-img' src={author} alt="Author" />
-                    <div className="blog-details">
-                        <h5>Author Name: {name}</h5>
-                        <p>{published_date} (5days ago)</p>
-                        <div className="read-time">
+                <img className='cover-img mt-4' src={cover} alt="Cover" />
+                <div className='flex items-center'>
+                    <img className='author-img rounded m-4' src={author} alt="Author" />
+                    <div className="blog-details flex justify-between items-center">
+                        <div className="author">
+                            <h5>Author Name: {name}</h5>
+                            <p>{published_date} (5days ago)</p>
+                        </div>
+                        <div className="read-time flex">
                             <p>{read_time} min read</p>
                             <img className='bookmark-ico' src="https://cdn-icons-png.flaticon.com/512/25/25667.png" alt="" />
                         </div>
                     </div>
-                    <h1>{title}</h1>
-                    <p>#hashtag</p>
-                    <a href="">Mark As Read</a>
                 </div>
+                <h1 className='text-4xl mt-4 mb-2'>{title}</h1>
+                <p className='mb-1'>#beginners #programming</p>
+                <a href="">Mark As Read</a>
             </div>
         </div>
     );
